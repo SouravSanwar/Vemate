36f6450c6aa1371497e29ed8088a480ab04b4aaf
Md Mostafizur Rahman <79080695+MostafizDev@users.noreply.github.com>
2022-04-04 21:15:45 +0600

    Bug fixes

--- lib/features/home/presentation/home.dart
+++ lib/features/home/presentation/home.dart
@@ -46,6 +46,8 @@ class _HomeState extends State<Home> {
 
     getData!.getVaultStats();
 
+    getData!.getCollectibles();
+
     super.initState();
   }
 
@@ -84,135 +86,124 @@ class _HomeState extends State<Home> {
         builder: (context, data, child) {
           return data.profileModel != null && data.vaultStatsModel != null
               ? SafeArea(
-                child: ListView(
-                  shrinkWrap: true,
-                  children: [
-                    Row(
-                      mainAxisAlignment:
-                          MainAxisAlignment.spaceBetween,
-                      children: [
-                        Row(
-                          children: [
-                            Padding(
-                                padding: const EdgeInsets.symmetric(
-                                    horizontal: 12.0, vertical: 5.0),
-                                child: SizedBox(
-                                  height: Get.height * .04,
-                                  width: Get.height * .04,
-                                  child: Image.asset(
-                                    'assets/media/icon/logo v.png',
-                                    fit: BoxFit.fill,
-                                  ),
-                                )),
-                            Text(
-                              "Hi, ${data.profileModel!.nickname.toString()}",
-                              style: Get.textTheme.headline1!
-                                  .copyWith(
-                                      color: AppColors.white,
-                                      fontWeight: FontWeight.w500),
-                            ),
-                          ],
-                        ),
-                        Padding(
-                          padding: const EdgeInsets.all(
-                            15.0,
-                          ),
-                          child: InkWell(
-                            onTap: () {
-                              Navigator.push(
-                                  context,
-                                  MaterialPageRoute(
-                                      builder: (c) =>
-                                          const Profile()));
-                            },
-                            child: Container(
-                              child: data.profileModel!
-                                          .profileImage !=
-                                      null
-                                  ? CircleAvatar(
-                                      radius: 15,
-                                      backgroundImage: NetworkImage(
-                                        Urls.mainUrl +
-                                            data
-                                                .profileModel!
-                                                .profileImage!
-                                                .mobile!
-                                                .src
-                                                .toString(),
-                                      ),
-                                    )
-                                  : const CircleAvatar(
-                                      radius: 15,
-                                      backgroundImage: AssetImage(
-                                          'assets/media/image/profile.png'),
+                  child: ListView(
+                    shrinkWrap: true,
+                    children: [
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: [
+                          Row(
+                            children: [
+                              Padding(
+                                  padding: const EdgeInsets.symmetric(
+                                      horizontal: 12.0, vertical: 5.0),
+                                  child: SizedBox(
+                                    height: Get.height * .04,
+                                    width: Get.height * .04,
+                                    child: Image.asset(
+                                      'assets/media/icon/logo v.png',
+                                      fit: BoxFit.fill,
                                     ),
-                              decoration: BoxDecoration(
-                                shape: BoxShape.circle,
-                                border: Border.all(
-                                  color: AppColors.greyWhite,
-                                  width: 1.0,
+                                  )),
+                              Text(
+                                "Hi, ${data.profileModel!.nickname.toString()}",
+                                style: Get.textTheme.headline1!.copyWith(
+                                    color: AppColors.white,
+                                    fontWeight: FontWeight.w500),
+                              ),
+                            ],
+                          ),
+                          Padding(
+                            padding: const EdgeInsets.all(
+                              15.0,
+                            ),
+                            child: InkWell(
+                              onTap: () {
+                                Navigator.push(
+                                    context,
+                                    MaterialPageRoute(
+                                        builder: (c) => const Profile()));
+                              },
+                              child: Container(
+                                child: data.profileModel!.profileImage != null
+                                    ? CircleAvatar(
+                                        radius: 15,
+                                        backgroundImage: NetworkImage(
+                                          Urls.mainUrl +
+                                              data.profileModel!.profileImage!
+                                                  .mobile!.src
+                                                  .toString(),
+                                        ),
+                                      )
+                                    : const CircleAvatar(
+                                        radius: 15,
+                                        backgroundImage: AssetImage(
+                                            'assets/media/image/profile.png'),
+                                      ),
+                                decoration: BoxDecoration(
+                                  shape: BoxShape.circle,
+                                  border: Border.all(
+                                    color: AppColors.greyWhite,
+                                    width: 1.0,
+                                  ),
                                 ),
                               ),
                             ),
                           ),
+                        ],
+                      ),
+                      Padding(
+                        padding: const EdgeInsets.symmetric(
+                            horizontal: 15.0, vertical: 5.0),
+                        child: Text(
+                          'News',
+                          textAlign: TextAlign.left,
+                          style: Get.textTheme.headline2!.copyWith(
+                              color: AppColors.white,
+                              fontWeight: FontWeight.w500),
                         ),
-                      ],
-                    ),
-
-                    Padding(
-                      padding: const EdgeInsets.symmetric(
-                          horizontal: 15.0, vertical: 5.0),
-                      child: Text(
-                        'News',
-                        textAlign: TextAlign.left,
-                        style: Get.textTheme.headline2!.copyWith(
-                            color: AppColors.white,
-                            fontWeight: FontWeight.w500),
                       ),
-                    ),
-                    AppSpaces.spaces_height_10,
-                    ImageSlider(),
-                    AppSpaces.spaces_height_10,
-                    Padding(
-                      padding: const EdgeInsets.only(
-                          top: 10, bottom: 0, left: 15, right: 15),
-                      child: Text(
-                        'My Vault',
-                        style: Get.textTheme.headline2!.copyWith(
-                            color: AppColors.white,
-                            fontWeight: FontWeight.w500),
+                      AppSpaces.spaces_height_10,
+                      ImageSlider(),
+                      AppSpaces.spaces_height_10,
+                      Padding(
+                        padding: const EdgeInsets.only(
+                            top: 10, bottom: 0, left: 15, right: 15),
+                        child: Text(
+                          'My Vault',
+                          style: Get.textTheme.headline2!.copyWith(
+                              color: AppColors.white,
+                              fontWeight: FontWeight.w500),
+                        ),
                       ),
-                    ),
-                    HomeVaultCard(
-                      vaultStatsModel: data.vaultStatsModel,
-                    ),
-
-                    Padding(
-                      padding: const EdgeInsets.only(
-                          top: 10, bottom: 10, left: 15, right: 15),
-                      child: Text(
-                        'Newest',
-                        style: Get.textTheme.headline1!.copyWith(
-                            color: AppColors.white,
-                            fontWeight: FontWeight.w500),
+                      HomeVaultCard(
+                        vaultStatsModel: data.vaultStatsModel,
                       ),
-                    ),
-
-                    SizedBox(
-                      height: Get.height * .22,
-                      child: SizedBox(
-                        width: Get.width,
-                        child: data.collectiblesModel != null
-                            ? VaultNewItemCard(
-                          list: data.collectiblesModel!.results,
-                        )
-                            : const LoadingExample(),
+                      Padding(
+                        padding: const EdgeInsets.only(
+                            top: 10, bottom: 10, left: 15, right: 15),
+                        child: Text(
+                          'Newest',
+                          style: Get.textTheme.headline1!.copyWith(
+                              color: AppColors.white,
+                              fontWeight: FontWeight.w500),
+                        ),
                       ),
-                    ),
-                    AppSpaces.spaces_height_10,
-                  ],
-                ),
-              )
+                      SizedBox(
+                        height: Get.height * .22,
+                        child: SizedBox(
+                          width: Get.width,
+                          child: data.collectiblesModel != null
+                              ? VaultNewItemCard(
+                                  list: data.collectiblesModel!.results,
+                                )
+                              : const LoadingExample(),
+                        ),
+                      ),
+                      AppSpaces.spaces_height_10,
+                    ],
+                  ),
+                )
               : const LoadingExample();
         },
       ),
