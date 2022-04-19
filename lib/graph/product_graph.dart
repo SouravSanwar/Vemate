24db92e44905d1dbea5bda48db4963dddea8085e
Md Mostafizur Rahman <79080695+MostafizDev@users.noreply.github.com>
2022-04-19 23:04:38 +0600

    Graph Improvement

--- lib/graph/product_graph.dart
+++ lib/graph/product_graph.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:ketemaa/core/Provider/getData.dart';
 import 'package:ketemaa/core/models/SingleProductModel.dart';
+import 'package:ketemaa/core/utilities/app_colors/app_colors.dart';
 import 'package:ketemaa/core/utilities/shimmer/loading.dart';
 import 'package:provider/provider.dart';
 import 'package:syncfusion_flutter_charts/charts.dart';
@@ -51,13 +52,13 @@ class _ProductGraphState extends State<ProductGraph> {
                 labelIntersectAction: AxisLabelIntersectAction.hide,
                 labelRotation: 0,
                 labelAlignment: LabelAlignment.start,
-                maximumLabels: 10,
+                maximumLabels: 24,
               ),
               tooltipBehavior: TooltipBehavior(enable: true),
               series: <ChartSeries<Graph, String>>[
-                LineSeries<Graph, String>(
-                  color: Colors.green,
+                SplineAreaSeries<Graph, String>(
                   dataSource: data.singleProductModel!.graph!,
+                  gradient: AppColors.graphGradient,
                   xValueMapper: (plot, _) => widget.graphType == '0'
                       ? plot.hourWiseTime
                       : plot.dayWiseTime,
