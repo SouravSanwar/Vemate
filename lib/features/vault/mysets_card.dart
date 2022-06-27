3081e5b22bc70363ed7a0e77190d7272352f2818
Sourav <sanwarulhaque1165@gmail.com>
2022-06-27 20:49:38 +0600

    image added in home and vault

--- lib/features/vault/mysets_card.dart
+++ lib/features/vault/mysets_card.dart
@@ -66,7 +66,8 @@ class _MysetsCardState extends State<MysetsCard> {
                       borderRadius: BorderRadius.circular(10),
                       border: Border.all(color: const Color(0xff454F70))),
                   child: data.setListModel!.results![index].setProductDetail!
-                              .image ==null
+                              .image!.image_on_list ==
+                          null
                       ? Text(
                           data.setListModel!.results![index].setProductDetail!.name
                               .toString()[0]
@@ -187,8 +188,7 @@ class _MysetsCardState extends State<MysetsCard> {
                                           Expanded(
                                             child: Text(
                                               r"$" +
-                                                  data
-                                                      .setListModel!
+                                                  data.setListModel!
                                                       .results![index]
                                                       .setProductDetail!
                                                       .priceChangePercent!
